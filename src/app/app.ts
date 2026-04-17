382ba2f3eb664de21c6bc6f13a9cc47f77b5aa28
madhu <madhuvayala9560@gmail.com>
2026-04-17 14:37:46 +0530

    color picker

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('deployement');
+
+  selectedColor = signal('#3498db');
+
+  updateColor(newColor: string) {
+    this.selectedColor.set(newColor);
+  }
+  copyColor(){
+    navigator.clipboard.writeText(this.selectedColor());
+    alert('Color code copied to clipboard: ' + this.selectedColor());
+  }
 }
